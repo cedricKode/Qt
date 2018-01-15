54289e4a6a1c49dfd609465e885565ed129f2de4
Mox812 <cedric.kode@gmail.com>
2018-01-15 18:01:13 +0000

    Update custumSlider.py

--- custumSlider.py
+++ custumSlider.py
@@ -6,49 +6,50 @@ import math
 
 
 class Slider(QtGui.QSlider):
-
-    def __init__(self, margin =5):
-
-
-        super(Slider, self).__init__()
-        self.default_value = 0
+    """Overide of the QSlider for having Nuke Style
+    Notice than it contains a ticks attribute that is a QWidget
+    To have the slider with the ticks, add th two in the same layout slot
+    """
+    def __init__(self, mini=0, maxi=100, color=None):
+        """Enumeration knob init
+        :param str mini: slider's maximum value
+        :param str maxi: slider's minimum value
+        :param str color: slider's color
+        """
+        super(Slider, self).__init__(QtCore.Qt.Horizontal)
+        self.mini = mini
+        self.width = float(maxi - mini)
+        self.setMinimum(mini * 1000)
+        self.setMaximum(maxi * 1000)
+        self.setValue(mini)
         self.setMouseTracking(True)
+        self.setTickPosition(QtGui.QSlider.TicksAbove)
+        self.setTickInterval((maxi - mini) / 10)
         self.setStyleSheet(self.stylesheet())
-        self.setOrientation(QtCore.Qt.Horizontal)
-        self.setTickPosition(QtGui.QSlider.TicksBothSides)
-        self.setTickInterval(margin)
-        self.setSingleStep(1)
-        self.setMouseTracking(True)
-        self.set_range((0,99))
 
 
+    def mousePressEvent(self, event):
+        """Overide the mousePressEvent for having the slider
+           which is following the mouse
+        """
+        value = self.width / self.size().width() * event.x() * 1000
+        value = value + self.mini * 1000
+        self.setValue(value)
+        super(Slider, self).mousePressEvent(event)
 
-#Layout
-    
-    def slider(self):
-        self.slider = Slider()        
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.slider)
-        self.setLayout(layout)
+    def emit_signal(self, value):
+        """Emit the knob signal on value change
+        """
+        self.signal.slider_clicked.emit(self, value)
 
-    def set_range(self, rng):
-        self._min = rng[0]
-        self._max = rng[1]
-        self.setMinimum(rng[0])
-        self.setMaximum(rng[1])
-        
-    def set_value(self,value):
-        self.default_value = value
-        
     def stylesheet(self):
-        return """ 
-      QSlider::groove:horizontal {
+        return """
+        QSlider::groove:horizontal {
         height: 4px;
         border: 1px solid black;
         border-radius: 3px;
         background: orange;
         }
-
         QSlider::handle:horizontal {
         border: 1px solid black;
         width: 3px;
@@ -57,18 +58,18 @@ class Slider(QtGui.QSlider):
         margin-top: -4px;
         margin-bottom: -4px;
         }
-
         QSlider::sub-page:horizontal {
-        background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,stop: 0 white, stop: 1 orange);
+        background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,stop: 0 white, stop: 1 orange);
         background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1, stop: 0 white, stop: 1 orange);
         height: 5px;
         border: 1px solid black;
         border-radius: 3px;
         }
-        
         """
 
-    
+
+
+   
 
 app = QtGui.QApplication(sys.argv)
 tool = Slider()
